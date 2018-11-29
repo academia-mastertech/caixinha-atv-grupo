c2515cceaf946a58f23f52192ffb24180bf7e287
Renan Rodrigo <renanrb@ime.usp.br>
2018-11-29 21:38:34 -0200

    Adicionar botao de colorir

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Caixinha from './Components/Caixinha/Caixinha';
+import AppStyle from './AppStyle';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div style={AppStyle.AppDiv}>
         <h1>Caixinhas para colorir</h1>
+        <button style={AppStyle.Button}>COLORIR! eeeee</button>
         <Caixinha />
       </div>
     )
---
+++ src/AppStyle.js
@@ -0,0 +1,16 @@
+const AppStyle = {
+  AppDiv:{
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column'
+  },
+  Button:{
+    height: 80,
+    width: 80,
+    borderRadius: 40,
+    border: '2px dashed black'
+  }
+}
+
+export default AppStyle;
